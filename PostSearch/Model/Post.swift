bf9aa95757070b73cc922d9bb20714895511317f
Ra01 <raul.aliaga.s@tecsup.edu.pe>
2022-03-14 10:00:44 -0500

    Create Model Post

--- PostSearch/Model/Post.swift
+++ PostSearch/Model/Post.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
-struct Post : Decodable{
+struct Post : Codable {
     
+    let id : Int
+    let userId : Int
+    let title : String
+    let body: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userId = "user_id"
+        case title, body
+    }
 }
