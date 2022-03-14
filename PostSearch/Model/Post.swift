6d97a3f47f913370a3e1a39ca0490cf1a245fb03
Ra01 <raul.aliaga.s@tecsup.edu.pe>
2022-03-14 09:38:42 -0500

    Initial Commit

---
+++ PostSearch/Model/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  PostSearch
+//
+//  Created by Raul Kevin Aliaga Shapiama on 14/03/22.
+//
+
+import Foundation
+
+struct Post : Decodable{
+    
+}
---
+++ PostSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostSearch
+//
+//  Created by Raul Kevin Aliaga Shapiama on 14/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
